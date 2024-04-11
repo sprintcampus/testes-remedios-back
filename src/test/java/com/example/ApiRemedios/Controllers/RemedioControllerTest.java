2c755c335d4f94dbd9632f5dfb1db3bc6d43b331
Amaro Elias <amaronascimento2016@hotmail.com>
2024-04-11 08:06:54 -0300

    iniciando os testes RemedioControllerTest

---
+++ src/test/java/com/example/ApiRemedios/Controllers/RemedioControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.ApiRemedios.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemedioControllerTest {
+
+    @Test
+    void cadastrar() {
+    }
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+
+    @Test
+    void reativar() {
+    }
+
+    @Test
+    void excluir() {
+    }
+
+    @Test
+    void inativar() {
+    }
+}
